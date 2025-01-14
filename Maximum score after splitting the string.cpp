96b7fd91cbe43073c563cdee66d42108a02aa9fc
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-01-14 12:11:15 +0530

    Maximum score after splitting the string.cpp

--- Maximum score after splitting the string.cpp
+++ Maximum score after splitting the string.cpp
@@ -24,3 +24,39 @@ public:
 
 //Better Approach
 //Time and Space complexity:
+class Solution {
+public:
+    int maxScore(string s) {
+int n=s.length();
+int result=INT_MIN;
+int total_ones=0;
+for(int i=0;i<n;i++){
+    if(s[i]=='1'){total_ones++;}
+}
+int zeros=0;
+int ones=0;
+for(int i=0;i<n-1;i++){
+    if(s[i]=='1'){ones++;}
+    else zeros++;
+    result=max(result,(zeros+total_ones-ones));
+}
+    return result;}
+};
+
+//Best Approach
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.length();
+        int score=INT_MIN;
+        int ones=0;
+        int zeros=0;
+        int total_ones=count(begin(s),end(s),'1');
+        for(int i=0;i<=n-2;i++){
+if(s[i]=='1') ones++;
+else zeros++;
+score=max(score,zeros-ones);
+        }
+        return score+total_ones;
+    }
+};
