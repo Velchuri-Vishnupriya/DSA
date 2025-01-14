5f933145daabc9ed46894a5bd8807863bb8e1577
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-01-14 12:02:19 +0530

    Construct K Palindrome Strings.cpp

---
+++ Construct K Palindrome Strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+      int n=s.length();
+      if(n<k){
+        return false;
+      } 
+      if(n==k){return true;}
+      vector<int> vec(26,0);
+      for(char& ch: s){
+        vec[ch-'a']++;
+      }
+      int oddCount=0;
+      for(int i=0;i<26;i++){
+        if(vec[i] %2!=0) oddCount++;
+      }
+      return oddCount<=k;
+    }
+};
