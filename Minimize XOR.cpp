6f0224cf6c23979e5d72882bdfd5df263b7bfc64
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-01-16 19:40:52 +0530

    Minimize XOR.cpp

--- Minimize XOR.cpp
+++ Minimize XOR.cpp
@@ -3,6 +3,36 @@
 //S.C : O(1);
 class Solution{
 public:
-
-
+bool isset(int& x,int bit){return x & (1<<bit);}
+bool setbit(int& x,int bit){return x|=(1<<bit);}
+bool unsetbit(int& x,int bit){return x&= ~(1<<bit);}
+    int minimizeXor(int num1, int num2) {
+ int x=num1;
+ int bit=0;
+ int requiredsetbits=__builtin_popcount(num2);
+ int currentsetbits=__builtin_popcount(num1);
+ if(currentsetbits < requiredsetbits){
+    while(currentsetbits<requiredsetbits){
+        if(!isset(x,bit)){
+            setbit(x,bit);
+            currentsetbits++;
+        }
+        bit++;
+    }
+ }
+ else if(currentsetbits>requiredsetbits){
+    while(currentsetbits > requiredsetbits){
+        if(isset(x,bit)){
+            unsetbit(x,bit);
+            currentsetbits--;
+        }
+        bit++;
+    }
+ }
+ return x;
+    }
 };
+
+//Approach-2
+//T.S:O()
+//S.C: O()
