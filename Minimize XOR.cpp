4e839b69628f9336831855433465e1d7134b56fe
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-01-16 20:13:14 +0530

    Minimize XOR.cpp

--- Minimize XOR.cpp
+++ Minimize XOR.cpp
@@ -34,5 +34,29 @@ bool unsetbit(int& x,int bit){return x&= ~(1<<bit);}
 };
 
 //Approach-2
-//T.S:O()
-//S.C: O()
+//T.S:O(1)
+//S.C: O(1)
+class Solution {
+public:
+bool isset(int& x,int bit){return x & (1<<bit);}
+bool setbit(int& x,int bit){return x|=(1<<bit);}
+bool unsetbit(int& x,int bit){return x&= ~(1<<bit);}
+    int minimizeXor(int num1, int num2) {
+int x=0;
+int requiredsetbits=__builtin_popcount(num2); //the function runs at log n complexity
+for(int i=31;i>0 && requiredsetbits>0;i--){
+    if(isset(num1,i)){
+        setbit(x,i);
+        requiredsetbits--;
+    }
+}
+for(int i=0;i<=31 && requiredsetbits>0;i++){
+    if(!isset(x,i)){
+        setbit(x,i);
+        requiredsetbits--;
+    }
+}
+ return x;
+    }
+};
+
