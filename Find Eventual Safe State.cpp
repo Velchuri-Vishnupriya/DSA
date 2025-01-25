ad88ff145d2fbb5fa26e7e2a305bffb950c3d304
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-01-25 13:07:45 +0530

    Find Eventual Safe State.cpp

--- Find Eventual Safe State.cpp
+++ Find Eventual Safe State.cpp
@@ -32,3 +32,41 @@ vector<bool>& inRecursion){
 };
 
 //Using BFS
+class Solution {
+public: vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+     int V=graph.size();
+     vector<vector<int>> adj(V);
+     queue<int> q;
+     vector<int> indegree(V,0);
+     for(int u=0;u<V;u++){
+       for(int& v:graph[u]){ 
+        adj[v].push_back(u);
+       indegree[u]++;
+       }
+     }
+     for(int i=0;i<V;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+     }  
+     vector<bool> safe(V,false);
+     while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        safe[u]=true;
+        for(int& v:adj[u]){
+            indegree[v]--;
+            if(indegree[v]==0){
+                q.push(v);
+            }
+        }
+     } 
+     vector<int> safenodes;
+     for(int i=0;i<V;i++){
+        if(safe[i]==true){
+            safenodes.push_back(i);
+        }
+     }
+     return safenodes;
+    }
+};
