d793b54de6cdc533984e1724f0184ec0232b8992
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-02-02 15:34:34 +0530

    Update Is Graph Bipartite.cpp

--- Is Graph Bipartite.cpp
+++ Is Graph Bipartite.cpp
@@ -1,5 +1,5 @@
+//Using DFS
 //T.S : O(V+E)
-
 class Solution {
 public:
     bool BP_DFS(vector<vector<int>>& graph, int val, vector<int>& color, int colorval) {
@@ -20,6 +20,41 @@ public:
         for (int i = 0; i < m; i++) {
             if (color[i] == -1) {  // Start DFS if node is uncolored
                 if (!BP_DFS(graph, i, color, 1)) return false;
+            }
+        }
+        return true;
+    }
+};
+
+//Using BFS
+//T.S: O(V+E)
+class Solution {
+public:
+    bool BP_BFS(vector<vector<int>>& graph, int val, vector<int>& color) {
+        queue<int> q;
+        q.push(val);
+        color[val]=1;
+        while(!q.empty()){
+            int val1=q.front();
+            q.pop();
+            
+            for(auto& ngbr: graph[val1]){
+                if(color[val1]==color[ngbr]) return false;
+                else if(color[ngbr]==-1)
+                {
+                    q.push(ngbr);
+                    color[ngbr]=1-color[val1];
+                }
+            }
+        }        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+      int m = graph.size();  // Number of nodes
+        vector<int> color(m, -1);  // Color array of size m
+
+        for (int i = 0; i < m; i++) {
+            if (color[i] == -1) {  // Start BFS if node is uncolored
+                if (!BP_BFS(graph, i, color)) return false;
             }
         }
         return true;
