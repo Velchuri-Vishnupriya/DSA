1d5c577b9a0f60cd418d081071058ba215ab917d
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-02-23 00:35:41 +0530

    Create Recover a tree from Preorder traversal.cpp

---
+++ Recover a tree from PreOrder traversal.cpp
@@ -0,0 +1,41 @@
+//Using recurrsion
+//T.C : O(n)
+//S.C: O(n) -> Recurrsion space stack
+
+class Solution {
+public:int n;
+TreeNode* solve(string& traversal,int& i,int depth){
+    if(i>=n) return NULL;
+    
+    int j=i;
+
+    while(j<n && traversal[j]=='-'){j++;}
+
+    int dashes=j-i;
+
+    if(depth != dashes) return nullptr;
+
+     i=j;
+
+int num;
+
+    while(i<n && isdigit(traversal[i]))
+    {
+        num=(num*10) + (traversal[i]-'0');
+i++;}
+
+    TreeNode* root=new TreeNode(num);
+
+    root->left = solve(traversal, i, depth + 1);
+root->right = solve(traversal, i, depth + 1);
+
+
+    return root;
+}
+    TreeNode* recoverFromPreorder(string traversal) {
+        n=traversal.length();
+        int i=0;
+        int depth=0;
+        return solve(traversal,i,depth);
+    }
+};
