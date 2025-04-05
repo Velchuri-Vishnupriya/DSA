e487c3ae067d11f03c8c6105acf2d220a05299df
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-04-05 20:39:11 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,23 @@
+//T.C:O(@^n)
+//S.C:O(2^n * length of each subset)
+//recurrsion uses system's space which is N here bcs depth of recurrsion tree will not be more than N
+class Solution {
+public:
+vector<vector<int>> result;
+void solve(int i,vector<int>&nums,vector<int>& temp)
+{
+    if(i>=nums.size()){
+        result.push_back(temp);
+    return ;}
+    //take
+    temp.push_back(nums[i]);
+    solve(i+1,nums,temp);
+    //not take
+    temp.pop_back();
+    solve(i+1,nums,temp);
+}    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        solve(0,nums,temp);
+        return result;
+    }
+};
