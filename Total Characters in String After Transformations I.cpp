fb98b6824b92d95b9d71a7e2a77dc6fa42733cfe
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-05-13 08:03:38 +0530

    Update Total Characters in String After Transformations I.cpp

--- Total Characters in String After Transformations I.cpp
+++ Total Characters in String After Transformations I.cpp
@@ -27,3 +27,39 @@ j++;
 };
 
 //Approach -2
+//T.C : O(n + t*26)
+//S.C : O(1) (vectors are of constant size)
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+const int M = 1e9 + 7;
+int n = s.length();
+vector<int> mp(26,0);
+
+for(char& ch: s)
+{
+    mp[ch-'a']++;
+} 
+
+for(int count=1; count<=t; count++){
+    vector<int>temp(26,0);
+    for(int i=0; i<=25; i++){
+        char ch= i + 'a' ;
+        int freq = mp[i];
+
+        if(ch != 'z'){
+            temp[(ch+1) - 'a']=(temp[(ch+1)-'a']+freq) % M;
+            }
+            else{
+                temp['a'-'a'] = (temp['a'-'a']+freq)%M;
+                temp['b'-'a'] = (temp['b'-'a']+freq)%M;
+            }
+    }
+    mp=move(temp); 
+}
+int result = 0 ;
+for(auto& it: mp){
+    result = (result + it)%M;
+}
+return result;}
+};
