e56d97e719273491c8df6404facd1c5d2a4f2235
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-06-07 00:05:23 +0530

    Update Find the Maximum Sum of Node Values.cpp

--- Find the Maximum Sum of Node Values.cpp
+++ Find the Maximum Sum of Node Values.cpp
@@ -19,3 +19,25 @@ public:
 else return sum;
     }
 };
+
+//Approach - 2
+//T.C: O(nlogn);
+//S.C: O(n);
+class Solution {
+public:
+    typedef long long ll;
+    ll maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edge)
+    {
+      vector<int> fayda;
+      ll normalsum =0;
+      for(int i=0; i<nums.size(); i++){
+        fayda.push_back((nums[i]^k) - nums[i]);
+        normalsum += (ll)  nums[i];
+      }
+      sort(fayda.begin(),fayda.end(),greater<int>());
+      for(int i=0;i<fayda.size()-1; i+=2){
+        if(fayda[i] + fayda[i+1] > 0) normalsum += (ll)(fayda[i]+fayda[i+1]);
+
+      }  
+      return normalsum;  }
+};
