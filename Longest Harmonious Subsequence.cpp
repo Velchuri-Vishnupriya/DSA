12d91e6e11d72a3e5e9757fb84d921b3290d6ed1
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-06-30 14:57:28 +0530

    Update Longest Harmonious Subsequence.cpp

--- Longest Harmonious Subsequence.cpp
+++ Longest Harmonious Subsequence.cpp
@@ -1,3 +1,4 @@
+//Approach - 1
 //T.C:O(n*log n)
 //S.C:O(1)
 class Solution {
@@ -15,5 +16,28 @@ for(int i=1; i<n; i++)
         len = max(len , c1+c2);
     }
 }  
+return len;  }
+};
+
+//Approach - 2
+//T.C:O(n)
+//S.C:O(n)
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int n = nums.size();
+        int len  = 0;
+        unordered_map<int,int> mp;
+        for(auto& num:nums){
+            mp[num]++;
+        }
+        for(auto& num:nums){
+            int minele = num;
+            int maxele = num+1;
+            
+            if(mp.count(maxele)){
+                len = max(len , (mp[minele]+ mp[maxele]));
+            }
+        }
 return len;  }
 };
