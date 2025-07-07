fba8d6b57d55df394bbd681c8508a51096c2e78b
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-07-07 09:32:41 +0530

    Update Find Lucky Integer in an Array.cpp

--- Find Lucky Integer in an Array.cpp
+++ Find Lucky Integer in an Array.cpp
@@ -16,4 +16,22 @@ public:
 };
 
 //Approach - 2
-//T.C: O()
+//T.c: O(n)
+//S.C: O(1)
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+     int n= arr.size();
+
+     for(int &num : arr){
+        int val = (num & 65535);
+     if(val <= n){
+        arr[val - 1] += (1<<16); //65536 ,pow(2,16)
+     }
+}
+
+for(int val = n; val>=1; val--){
+    if((arr[val-1]>>16) == val) return val;
+}
+   return -1;}
+};
