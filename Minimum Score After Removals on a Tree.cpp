b74e4d5c3267db94bfb49f94fb30ff35f3091f50
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-08-06 00:47:14 +0530

    Create Minimum Score After Removals on a Tree.cpp

---
+++ Minimum Score After Removals on a Tree.cpp
@@ -0,0 +1,65 @@
+//T.C : O(n^2)
+class Solution {
+public:
+    void dfs(int node, int parent, vector<int>& subtreeXor, vector<int>& inTime, vector<int>& outTime, int& time, vector<int>& nums, unordered_map<int,vector<int>>& adj) {
+        subtreeXor[node] = nums[node];
+        inTime[node] = time++;
+        
+        for(int& ngbr : adj[node]) {
+            if(ngbr != parent) {
+                dfs(ngbr, node, subtreeXor, inTime, outTime, time, nums, adj);
+                subtreeXor[node] ^= subtreeXor[ngbr];
+            }
+        }
+        outTime[node] = time++;
+    }
+
+    bool isAncestor(int u, int v, vector<int>& inTime, vector<int>& outTime) {
+        return inTime[u] <= inTime[v] && outTime[v] <= outTime[u];
+    }
+
+    int getScore(int a, int b, int c) {
+        return max({a, b, c}) - min({a, b, c});
+    }
+
+    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> adj;
+
+        for(auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> subtreeXor(n, 0), inTime(n, 0), outTime(n, 0);
+        int timer = 0;
+
+        dfs(0, -1, subtreeXor, inTime, outTime, timer, nums, adj);
+
+        int result = INT_MAX;
+        for(int u = 1; u < n; u++) {
+            for(int v = u + 1; v < n; v++) {
+                int xor1, xor2, xor3;
+
+                if(isAncestor(u, v,inTime,outTime)) {
+                    xor1 = subtreeXor[v];
+                    xor2 = subtreeXor[u] ^ subtreeXor[v];
+                    xor3 = subtreeXor[0] ^ subtreeXor[u];
+                } else if(isAncestor(v, u,inTime,outTime)) {
+                    xor1 = subtreeXor[u];
+                    xor2 = subtreeXor[v] ^ subtreeXor[u];
+                    xor3 = subtreeXor[0] ^ subtreeXor[v];
+                } else {
+                    xor1 = subtreeXor[u];
+                    xor2 = subtreeXor[v];
+                    xor3 = subtreeXor[0] ^ xor1 ^ xor2;
+                }
+
+                result = min(result, getScore(xor1, xor2, xor3));
+            }
+        }
+
+        return result;
+    }
+};
