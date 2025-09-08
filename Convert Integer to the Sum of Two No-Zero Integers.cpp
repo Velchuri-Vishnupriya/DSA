8519e97abe7f2a860e7acc3cfab4ae6f322f248f
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-09-08 22:17:56 +0530

    Update Convert Integer to the Sum of Two No-Zero Integers.cpp

--- Convert Integer to the Sum of Two No-Zero Integers.cpp
+++ Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -1,3 +1,4 @@
+//Brute Force
 //T.C : O(n*log n)
 class Solution {
 public:
@@ -11,3 +12,30 @@ public:
     return {1,1};
     }
 };
+
+//Optimal
+//T.C : O(log n)
+//T.C : O(log n)
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        int a = n;
+        int b = 0;
+        int placeValue = 1;
+
+        // a + b = n
+        while (n > 1) {
+            int take = 1;
+            if (n % 10 == 1) take = 2;  // avoid zero digit
+
+            a -= take * placeValue;
+            b += take * placeValue;
+
+            n = (n - take )/10;               // move to next digit
+            placeValue *= 10;
+        }
+
+        return {a, b};
+    }
+};
+
