1b7cc267f50b4afd8a48bc42259593e73e8de797
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-02 09:37:33 +0530

    Update Valid Triangle.cpp

--- Valid Triangle.cpp
+++ Valid Triangle.cpp
@@ -66,5 +66,34 @@ int binarySearch(vector<int>& nums, int l, int r, int target){
 };
 
 //Approach - 3
-//T.C : O()
+//T.C : O(n^2)
+class Solution {
+public:
 
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        if( n < 3) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int count = 0;
+
+        for(int k = n-1; k >= 2; k--){//O(n)
+            int i=0;
+            int j=k-1;
+
+            //nums[i] + nums[j] > nums[k]
+            while(i < j){//at max O(n-2)
+                if(nums[i] + nums[j] > nums[k]){
+                    count += (j - i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+
+  return count;  }
+};
