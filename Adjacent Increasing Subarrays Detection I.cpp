24f46f66429bdee32f7d94e2e4eacd16138e8ffc
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-14 19:58:19 +0530

    Create Adjacent Increasing Subarrays Detection I.cpp

---
+++ Adjacent Increasing Subarrays Detection I.cpp
@@ -0,0 +1,37 @@
+//T.C: O(n*k)
+//S.C: O(1)
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        // there must exist at least 2*k elements
+        if (n < 2 * k) return false;
+
+        for (int i = 0; i + 2 * k - 1 < n; i++) {//O(n - 2*k + 1)
+            int a = i;
+            bool Found = true;
+
+            // 1st subarray
+            for (int j = a; j < a + k - 1; j++) {
+                if (nums[j] >= nums[j + 1]) {
+                    Found = false;
+                    break;
+                }
+            }
+            if (!Found) continue;
+
+            // 2nd subarray
+            for (int j = a + k; j < a + 2 * k - 1; j++) {
+                if (nums[j] >= nums[j + 1]) {
+                    Found = false;
+                    break;
+                }
+            }
+
+            if (Found) return true;
+        }
+
+        return false;
+    }
+};
