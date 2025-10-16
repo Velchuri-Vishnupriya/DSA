e300c7a0c2e2b1a9b1dce831ba8bc2910ea0d581
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-16 21:22:17 +0530

    Update Find Resultant Array After Removing Anagrams.cpp

--- Find Resultant Array After Removing Anagrams.cpp
+++ Find Resultant Array After Removing Anagrams.cpp
@@ -18,3 +18,39 @@ public:
         }
    return result; }
 };
+
+//Approach - 2
+//n = number of words
+//m = average (or max) length of each word
+//T.C : O(m*n)
+//S.C : O(1)
+class Solution {
+public:
+bool isAnagram(string s1, string s2){//O(m)
+    vector<int> arr(26,0);
+
+    for(int i = 0; i < s1.length(); i++){
+        arr[s1[i] - 'a']++;
+    }
+
+    for(int i = 0; i < s2.length(); i++){
+        arr[s2[i] - 'a']--;
+    }
+
+    for(int i = 0; i < 26; i++){
+        if(arr[i] != 0) return false;
+    }
+    return true;
+}
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> result;
+        result.push_back(words[0]);
+
+       int n = words.size();
+
+       for(int i = 1; i < n; i++){//O(n)
+        if(!isAnagram(result.back(), words[i])) result.push_back(words[i]); //O(m)
+       }
+   return result; }
+};
+
