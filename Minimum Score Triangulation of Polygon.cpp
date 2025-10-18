d3ebebe499d4511a62f111262ef568c39c82152c
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-18 21:37:03 +0530

    Create  Minimum Score Triangulation of Polygon.cpp

---
+++ Minimum Score Triangulation of Polygon.cpp
@@ -0,0 +1,23 @@
+//T.C: O(n^3)
+//S.C: O(n^2)
+class Solution {
+public:
+int t[51][51];//O(n^2)
+int solve(vector<int>& values, int i, int j){
+    if(j - i + 1 < 3) return 0;
+    if(t[i][j] != -1) return t[i][j];
+    int result = INT_MAX;
+    for(int k = i + 1; k < j; k++){//O(n)
+        int wt = values[i] * values[k] * values[j] 
+        + solve(values, i, k)
+        + solve(values, k, j);
+        result = min (result, wt);
+    }
+    return t[i][j] = result;
+}
+    int minScoreTriangulation(vector<int>& values) {
+       int n = values.size();
+       memset(t, -1, sizeof(t));
+       return solve(values, 0, n-1);
+    }
+};
