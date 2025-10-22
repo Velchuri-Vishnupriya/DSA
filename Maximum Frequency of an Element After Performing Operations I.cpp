d830e51f115760a506871a69ea93a8eeee3f3884
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-23 00:03:26 +0530

    Create Maximum Frequency of an Element After Performing Operations I.cpp

---
+++ Maximum Frequency of an Element After Performing Operations I.cpp
@@ -0,0 +1,33 @@
+//T.C : O(maxEl)
+//S.C : O(maxEl)
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+     int maxEl = *max_element(begin(nums), end(nums)) + k;
+
+     vector<int>freq(maxEl+1, 0);
+
+     for(int &num: nums){
+        freq[num]++;
+     }
+
+     //cum freq array
+     for(int i = 1; i <= maxEl; i++){
+        freq[i] += freq[i-1];
+     }
+
+     int result = 0;
+     for(int target = 0; target <= maxEl; target++){
+        if(freq[target] == 0) continue;
+
+        int leftNum = max(0, target - k);
+        int rightNum = min(maxEl, target + k);
+
+        int totalCount = freq[rightNum] - ( leftNum > 0 ? freq[leftNum - 1] : 0);
+        int targetCount = freq[target] -(target > 0 ? freq[target - 1] : 0);
+
+        int need = abs(totalCount - targetCount);
+        result = max(targetCount + min(need, numOperations), result);
+     }
+     return result;}
+};
