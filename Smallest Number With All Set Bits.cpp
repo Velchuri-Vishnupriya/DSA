4f466c826e771b32eb2209568966e801d4c9c58f
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-10-29 18:49:00 +0530

    Update Smallest Number With All Set Bits.cpp

--- Smallest Number With All Set Bits.cpp
+++ Smallest Number With All Set Bits.cpp
@@ -11,3 +11,43 @@ public:
        return vec[i];
     }
 };
+
+//T.C : O(log n) n = size of vector 
+//S.C : O(1)
+class Solution {
+public:
+bool isResult(int x){
+    return (x & (x+1)) == 0;
+}
+    int smallestNumber(int n) {
+       int result = n;
+
+       while(!isResult(result)){
+        result++;
+       }
+       return result;
+    }
+};
+
+//T.C : O(log n) n = size of vector 
+//S.C : O(1)
+class Solution {
+public:
+int smallestNumber(int n) {
+       int result = 1;
+
+       while(result < n){
+        result = 2*result + 1;
+       }
+    return result;}
+};
+
+//T.C : O(log n) n = size of vector 
+//S.C : O(1)
+class Solution {
+public:
+int smallestNumber(int n) {
+       int bits = log2(n) + 1;
+       return (1 << bits) -1;
+       }
+};
