cce07239d23620d9f8190b21ac662402255566a5
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-11-27 10:14:50 +0530

    Update Paths in Matrix Whose Sum Is Divisible by K.cpp

--- Paths in Matrix Whose Sum Is Divisible by K.cpp
+++ Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -27,3 +27,40 @@ int solve(int i, int j, int currSum, vector<vector<int>>& grid, int k){
 };
 
 //Bottom-Up
+//T.C : O(m*n*k)
+//S.C : O(m*n*k)
+class Solution {
+public:
+    int M = 1e9 + 7;
+
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<vector<int>>> t(m+1, vector<vector<int>>(n+1, vector<int>(k, 0)));
+
+        // Base case at destination
+        for (int remain = 0; remain < k; remain++) {
+            t[m-1][n-1][remain] = ((remain + grid[m-1][n-1]) % k == 0);
+        }
+
+        // Bottom-up DP
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                
+                if (i == m-1 && j == n-1) continue;
+
+                for (int remain = 0; remain < k; remain++) {
+                    int R = (remain + grid[i][j]) % k;
+
+                    int down = (i+1 < m) ? t[i+1][j][R] : 0;
+                    int right = (j+1 < n) ? t[i][j+1][R] : 0;
+
+                    t[i][j][remain] = (down + right) % M;
+                }
+            }
+        }
+
+        return t[0][0][0];
+    }
+};
