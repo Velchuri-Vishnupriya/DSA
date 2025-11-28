6147bfc2c222f264334c4b6020c9a93797dcf6f3
Velchuri Vishnupriya <vp0801892@gmail.com>
2025-11-28 12:26:47 +0530

    Create Substring With Largest Variance.cpp

---
+++ Substring With Largest Variance.cpp
@@ -0,0 +1,48 @@
+//T.C : O(26*26*n)~O(n)
+//S.C : O(26) ~ O(1)
+class Solution {
+public:
+    int largestVariance(string s) {
+        vector<int> count(26,0);
+
+        for(char &ch : s){
+            count[ch - 'a'] = 1;
+        }
+
+        int result = 0;
+
+        for(char first = 'a'; first <= 'z'; first++){
+            for(char second = 'a'; second <= 'z'; second++){
+
+                if(count[first - 'a'] == 0 || count[second - 'a'] == 0) continue;
+
+                int firstCount = 0; //F1
+                int secondCount = 0; //F2
+
+                bool pastSecond = 0;
+
+                for(char &ch : s){
+                    if(ch == first){
+                        firstCount++;
+                }else if(ch == second){
+                    secondCount++;
+                    }
+
+                    if(secondCount > 0){
+                        result = max(result, firstCount - secondCount);
+                    }else{
+                        if(pastSecond == true){
+                            result = max(result, firstCount - 1);
+                        }
+                    }
+
+                    if(secondCount > firstCount){
+                        secondCount = 0;
+                        firstCount = 0;
+                        pastSecond = true;
+                    }
+                }
+            }
+        }
+   return result; }
+};
