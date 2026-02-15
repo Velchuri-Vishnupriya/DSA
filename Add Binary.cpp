0ebecc962f295d11d25479816de9fb4172be8c7c
Velchuri Vishnupriya <vp0801892@gmail.com>
2026-02-15 09:36:33 +0530

    Create Add Binary.cpp

---
+++ Add Binary.cpp
@@ -0,0 +1,27 @@
+//T.C: O(max(len(a), len(b))
+//S.C : O(1)
+class Solution {
+public:
+    string addBinary(string a, string b) {
+       int i = a.length()-1;
+       int j = b.length()-1;
+       int carry = 0;
+
+       string ans = "";
+
+       while(i >= 0 || j >= 0 || carry){
+        int sum = carry;
+
+        if(i >= 0) sum += (a[i] - '0');
+        if(j >= 0) sum += (b[j] - '0');
+
+        ans.push_back(sum % 2 + '0');
+        carry = sum/2;
+
+        i--; j--;
+       }
+
+       reverse(ans.begin(),ans.end());
+       return ans;
+    }
+};
