4cb58fa3c24d4a6921fd3ab0c83e60df7490518c
Velchuri Vishnupriya <vp0801892@gmail.com>
2026-02-27 23:46:04 +0530

    Create Two Sum II - Input Array Is Sorted.cpp

---
+++ Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,17 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int left = 0;
+        int right = n-1;
+
+        while(left < right){
+            if(numbers[left] + numbers[right] == target) return {left+1, right+1};
+            else if(numbers[left] + numbers[right] < target) left++;
+            else right--;
+        }
+    return {-1,-1};
+    }
+};
